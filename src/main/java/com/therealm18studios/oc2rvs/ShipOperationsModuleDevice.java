4945fae10995592143be027f726650f501890e71
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-09-06 19:28:33 -0500

    Clean up and combined methods in the Ship Operations Module

--- src/main/java/com/therealm18studios/oc2rvs/ShipOperationsModuleDevice.java
+++ src/main/java/com/therealm18studios/oc2rvs/ShipOperationsModuleDevice.java
@@ -1,14 +1,16 @@
 package com.therealm18studios.oc2rvs;
 import li.cil.oc2.common.bus.device.rpc.item.*;
 import net.minecraft.world.item.ItemStack;
-import org.joml.Vector3d;
-import org.joml.primitives.AABBi;
+import org.joml.Vector3dc;
 
 import java.util.*;
 
 import li.cil.oc2.api.bus.device.object.Callback;
 import li.cil.oc2.api.bus.device.object.DocumentedDevice;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import org.joml.primitives.AABBic;
+import org.valkyrienskies.core.api.ships.properties.ShipInertiaData;
+import org.valkyrienskies.core.api.ships.properties.ShipTransform;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.core.api.ships.ServerShip;
 
@@ -37,222 +39,87 @@ public final class ShipOperationsModuleDevice extends AbstractItemRPCDevice impl
         }
     }
 
-
     @Callback(name = "getId")
     public long getId(){
         verify();
         return this.ship.getId();
     }
 
-    @Callback(name = "getMass")
-    public double getMass() {
+    @Callback(name = "getInertiaData")
+    public ShipInertiaData getInertiaData() {
         verify();
-        return this.ship.getInertiaData().getShipMass();
+        return this.ship.getInertiaData();
     }
 
-//    @Callback(name = "getMomentOfInertiaTensorToSave")
-//    public List<List<Double>> getMomentOfInertiaTensorToSave() {
-//        verify();
-//        return this.ship.getInertiaData().getMomentOfInertiaTensor();
-//     }
-//
-//    @Callback(name = "getMomentOfInertiaTensor")
-//    public List<List<Double>> getMomentOfInertiaTensor() {
-//        verify();
-//        return this.ship.inertiaData.momentOfInertiaTensor;
-//    }
-
-    @Callback(name = "getName")
-    public String getName() {
+    @Callback(name = "getSlug")
+    public String getSlug() {
         verify();
         return this.ship.getSlug();
     }
 
-
     @Callback(name = "getOmega")
-    public Map<String, Double> getOmega() {
+    public Vector3dc getOmega() {
         verify();
-        final var o = this.ship.getOmega();
-        return Map.of(
-                      "x", o.x(),
-                      "y", o.y(),
-                      "z", o.z()
-                      );
+        return this.ship.getOmega();
     }
 
-    @Callback(name = "getEulerAnglesXYZ")
-    public Map<String, Double> getEulerAnglesXYZ() {
+    @Callback(name = "getShipTransform")
+    public ShipTransform getShipTransform() {
         verify();
-        final var o = new Vector3d();
-        this.ship.getTransform().getShipToWorldRotation().getEulerAnglesXYZ(o);
-        return Map.of(
-                      "x", o.x(),
-                      "y", o.y(),
-                      "z", o.z()
-                      );
-
+        return this.ship.getTransform();
     }
 
-
-    @Callback(name = "getScale")
-    public Map<String, Double> getScale() {
+    @Callback(name = "getShipAABB")
+    public AABBic getShipAABB() {
         verify();
-        final var o = this.ship.getTransform().getShipToWorldScaling();
-        return Map.of(
-                      "x", o.x(),
-                      "y", o.y(),
-                      "z", o.z()
-                      );
-
-    }
-
-    @Callback(name = "getShipyardPosition")
-    public Map<String, Double> getShipyardPosition() {
-        verify();
-        final var o = this.ship.getTransform().getPositionInShip();
-        return Map.of(
-                      "x", o.x(),
-                      "y", o.y(),
-                      "z", o.z()
-                      );
-
-    }
-
-    @Callback(name = "getSize")
-    public Map<String, Object> getSize() {
-        verify();
-        var aabb = this.ship.getShipAABB();
-        if (aabb == null) {
-            aabb = new AABBi(0, 0, 0, 0, 0, 0);
-        }
-        return Map.of(
-                     "x", aabb.maxX() - aabb.minX(),
-                     "y", aabb.maxY() - aabb.minY(),
-                     "z", aabb.maxZ() - aabb.minZ()
-                     );
+        return this.ship.getShipAABB();
     }
 
     @Callback(name = "getVelocity")
-    public Map<String, Double> getVelocity() {
-        final var o = this.ship.getVelocity();
-        return Map.of(
-                      "x", o.x(),
-                      "y", o.y(),
-                      "z", o.z()
-                      );
-
+    public Vector3dc getVelocity() {
+        verify();
+        return this.ship.getVelocity();
     }
-
-    @Callback(name = "getWorldspacePosition")
-    public Map<String, Double> getWorldspacePosition() {
-        final var o = this.ship.getTransform().getPositionInWorld();
-        return Map.of(
-                      "x", o.x(),
-                      "y", o.y(),
-                      "z", o.z()
-                      );
-    }
-
 
     @Callback(name = "isStatic")
     public boolean isStatic() {
         return this.ship.isStatic();
     }
 
-    @Callback(name = "setName")
-    public void setName(final String name) {
-        this.ship.setSlug(name);
+    @Callback(name = "setSlug")
+    public void setSlug(final String slug) {
+        this.ship.setSlug(slug);
     }
-
-
-    /*
-    @LuaFunction
-    fun doFluidDrag() = this.physShip.doFluidDrag
-
-    @LuaFunction
-    fun getInertia(): Map<String, Any> {
-        val inertia = this.physShip.inertia
-        return mapOf(
-            Pair("momentOfInertiaTensor", inertia.momentOfInertiaTensor.toLua()),
-            Pair("mass", inertia.shipMass)
-        )
-    }
-
-    @LuaFunction
-    fun getPoseVel(): Map<String, Map<String, Double>> {
-        val poseVel = this.physShip.poseVel
-        return mapOf(
-            Pair("vel", poseVel.vel.toLua()),
-            Pair("omega", poseVel.omega.toLua()),
-            Pair("pos", poseVel.pos.toLua()),
-            Pair("rot", poseVel.rot.toLua())
-        )
-    }
-
-    */
 
     @Override
     public void getDeviceDocumentation(final DocumentedDevice.DeviceVisitor visitor) {
         visitor.visitCallback("onShip")
-            .description("Check if the ship interface is on an assembled ship. ")
-            .returnValueDescription("true if on a ship, otherwise false.");
+                .description("Check if the ship interface is on an assembled ship.")
+                .returnValueDescription("true if on a ship, otherwise false.");
         visitor.visitCallback("getId")
-            .description("Gives the numeric id of the ship")
-            .returnValueDescription("The numeric id of the ship")
-            ;
-        visitor.visitCallback("getMass")
-            .description("Returns the mass of the ship")
-            .returnValueDescription("The mass of the ship")
-            ;
-        visitor.visitCallback("getName")
-            .description("Returns the name of the ship")
-            .returnValueDescription("The name of the ship")
-            ;
+                .description("Gives the numeric id of the ship")
+                .returnValueDescription("The numeric id of the ship");
+        visitor.visitCallback("getInertiaData")
+                .description("Returns the inertia data of the ship")
+                .returnValueDescription("The inertia data of the ship");
+        visitor.visitCallback("getSlug")
+                .description("Returns the slug of the ship")
+                .returnValueDescription("The slug of the ship");
         visitor.visitCallback("getOmega")
-            .description("Returns the omega of the ship as Euler angles")
-            .returnValueDescription("The rotational velocity of the ship")
-            ;
-        visitor.visitCallback("getEulerAnglesXYZ")
-            .description("Returns the rotation of the ship as euler angles")
-            .returnValueDescription("The rotation of the ship")
-            ;
-        visitor.visitCallback("getScale")
-            .description("Returns the scale factor of the ship")
-            .returnValueDescription("The scale of the ship")
-            ;
-        visitor.visitCallback("getShipyardPosition")
-            .description("Returns the position of the ship in the shipyard")
-            .returnValueDescription("The position of the ship")
-            ;
-        visitor.visitCallback("getSize")
-            .description("Returns the AABB size of the ship")
-            .returnValueDescription("The AABB size of the ship")
-            ;
+                .description("Returns the omega of the ship as Euler angles")
+                .returnValueDescription("The rotational velocity of the ship");
+        visitor.visitCallback("getShipAABB")
+                .description("Returns the AABB of the ship")
+                .returnValueDescription("The AABB of the ship");
         visitor.visitCallback("getVelocity")
-            .description("Returns the velocity of the ship")
-            .returnValueDescription("The velocity of the ship")
-            ;
-
-        visitor.visitCallback("getWorldspacePosition")
-            .description("Returns the position of the ship in the world")
-            .returnValueDescription("The position of the ship")
-            ;
+                .description("Returns the velocity of the ship")
+                .returnValueDescription("The velocity of the ship");
         visitor.visitCallback("isStatic")
-            .description("Check if the ship is currently an active physics object")
-            .returnValueDescription("false if the ship is asleep")
-            ;
-        visitor.visitCallback("getBuoyantfactor")
-            .description("If the ship is a PhysShip, gets its buoyancy")
-            .returnValueDescription("The buoyancy factor, -1 if the ship is not a PhysShip")
-            ;
-        visitor.visitCallback("setName")
-            .description("Sets the name of the ship as a string")
-            ;
-
-
-
-
-
+                .description("Check if the ship is currently an active physics object")
+                .returnValueDescription("false if the ship is asleep");
+        visitor.visitCallback("setSlug")
+                .parameterDescription("slug", "The new slug")
+                .description("Sets the slug of the ship as a string");
     }
 
     ///////////////////////////////////////////////////////////////////
